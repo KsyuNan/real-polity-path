6c9f6b64656593b912c6812be738f091f3bbfa8f
Oksana Nanieva <54180932+KsyuNan@users.noreply.github.com>
2025-09-02 17:16:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/real-polity-path/",
+  base: '/real-polity-path/',
   server: {
     host: "::",
     port: 8080,
